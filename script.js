92c5de571052c5ded02e8be67e07b03ce8b8b2d8
Johnson <reclaimyourdata@tutamail.com>
2019-07-04 16:36:24 +0200

    cursor update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const cursor = document.querySelector('.cursor');
 
 document.addEventListener('mousemove', e => {
-    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+    cursor.setAttribute("style", "top: "+(e.clientY - 10)+"px; left: "+(e.clientX - 10)+"px;")
 })
 document.addEventListener('click', () => {
     cursor.classList.add("expand");
