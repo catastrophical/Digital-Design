03bb70b92d3c75b4d17a0436cb19dcc0a2576dce
Kevin Lindemark <kevinlindemark@gmail.com>
2019-09-22 20:53:44 +0200

    New elements and smoother graphics

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-// cursor animated https://www.youtube.com/watch?v=rfpRZ2t_BrQ
-const cursor = document.querySelector('.cursor');
+const chartWrapper = document.querySelector(".chart-wrapper");
+const scrollDown = document.querySelector(".scroll-down");
 
-document.addEventListener('mousemove', e => {
-    cursor.setAttribute("style", "top: "+(e.clientY - 10)+"px; left: "+(e.clientX - 10)+"px;")
-})
-document.addEventListener('click', () => {
-    cursor.classList.add("expand");
+function isElementInViewport(el) {
+ var rect = el.getBoundingClientRect();
+ return (
+   rect.top >= 0 &&
+   rect.left >= 0 &&
+   rect.bottom <=
+     (window.innerHeight || document.documentElement.clientHeight) &&
+   rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+ );
+}
 
-    setTimeout(() => {
-        cursor.classList.remove("expand");
-    }, 500)
+window.addEventListener("scroll", scrollHandler);
 
-})
+function scrollHandler() {
+ window.pageYOffset > 0
+   ? scrollDown.classList.add("is-hidden")
+   : scrollDown.classList.remove("is-hidden");
+ if (isElementInViewport(chartWrapper)) chartWrapper.classList.add("in-view");
+}
+
 
   // text animaton
 var TxtRotate = function(el, toRotate, period) {
